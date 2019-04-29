c6d93df8c0f79128754555fc8caeac64c7fc6129
Sadan Mallhi <smallhi@chathamfinancial.com>
2019-04-29 07:42:56 -0400

    separating the deploy and publish

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# PUBLISH
-
-# Bash Variables
-# DOCKERHUB_USERNAME (env var) (update var name)
-# DOCKERHUB_PASSWORD (env var) (update var name)
-# DOCKERHUB_REPO (local) (create this)
-# DOCKERHUB_TAG (local) (create this)
-
-# Build Docker Image
-docker build -f ./Dockerfile.prod -t mrplum/react_portfolio .
-
-# Login in to push to docker hub
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-
-# Push to docker hub with tag
-docker push mrplum/react_portfolio:latest
-
 # DEPLOY
 
 # Bash Variables
